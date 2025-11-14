dbe04782e87c3f1add47142a3be45046b0c3c2b5
Flip546 <filipbrzaz@gmail.com>
2025-11-14 12:30:04 +0100

    21

---
+++ 04-Functions/3-1.py
@@ -0,0 +1,13 @@
+###
+# To use the functions available in an external module,
+# you must include that module in your program.
+import math
+
+square_root = math.sqrt(7)
+print(f'A square root of 7 is', {square_root})
+
+logar = math.log1p(5)
+print(f'Natural logarythm of 5 is {logar}')
+ 
+sinus  = math.sin(90)
+print (f'Sine of 90 is {sinus}')
