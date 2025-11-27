d85ed87ac88c4d20a284a2ef278a8f818a530b7d
Filip Brzazgacz <filipbrzaz@gmail.com>
2025-11-27 17:41:50 +0100

    1

--- 04-Functions/testing/p1.py
+++ 04-Functions/testing/p1.py
@@ -1,13 +1,3 @@
-coin1 = 1
-coin2 = 2
-coin3 = 5
-
-def  f(amount_to_pay):
-    amount1 = (amount_to_pay // coin3)
-    rest1 = (amount_to_pay % coin3)
-    amount2 = (rest1 // coin2)
-    rest2 = (amount2 %  coin2)
-    amount3 = (rest2 // coin1)
-    full = (amount1 + amount2 + amount3)
-    return full
+def f(n):
+    return n//5 + (n%5)//2 + (n%5%2)
 
