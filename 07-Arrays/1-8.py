0b1d7b5fbd59aa4154c7cd6c2871f32cf5914d04
Filip Brzazgacz <filipbrzaz@gmail.com>
2025-12-03 11:49:17 +0100

    112

--- 07-Arrays/1-8.py
+++ 07-Arrays/1-8.py
@@ -3,5 +3,13 @@ computer_games = [
    "League of Legends", "Valorant", "Grand Theft Auto V", 
    "Elden Ring", "Apex Legends", "Call of Duty: Warzone"
 ]
+computer_games.sort()
+game_number = 1
+i = 0
+while i < len(computer_games):
+    game_name = computer_games[i]
+    i += 1
+    print(f'{i}  {game_name}')
+
 
 
