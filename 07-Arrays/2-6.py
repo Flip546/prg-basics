97d3ef87a33c83ea45884437cbcc065840fe8622
Filip Brzazgacz <filipbrzaz@gmail.com>
2025-12-04 21:10:59 +0100

    2.6

---
+++ 07-Arrays/2-6.py
@@ -0,0 +1,20 @@
+matrix = [
+   [0,0,0],
+   [0,0,0],
+   [0,0,0]
+]
+matrix [0][0] = 1
+matrix [1][1] = 1
+matrix [2][2] = 1
+
+for row in matrix:
+    for s in row:
+       
+        print(s, end=" ")
+
+    # ... Twój kod z pętlami ...
+
+    print() 
+
+
+
